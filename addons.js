43eb02254da5e21c6190b7bfe397b8a4446a2fb4
Findlad <findlad@gmail.com>
2024-02-27 10:05:03 -0700

    Added sugar, but no coop. Theres a shortage

---
+++ addons.js
@@ -0,0 +1,13 @@
+let swSugarPrice;
+let swSugarPage = "https://voila.ca/products/508600EA/details";
+let ssSugarPrice;
+let ssSugarPage =
+  "https://www.realcanadiansuperstore.ca/granulated-white-sugar/p/20032352_EA";
+let wmSugarPrice;
+let wmSugarPage =
+  "https://www.walmart.ca/en/ip/Redpath-Special-Fine-Granulated-Sugar/10053833";
+let nfSugarPrice;
+let nfSugarPage =
+  "https://www.nofrills.ca/granulated-white-sugar/p/20032352_EA";
+let coopSugarPrice;
+let coopSugarPage = "https://voila.ca/products/732323EA/details";
